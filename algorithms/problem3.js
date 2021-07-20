8ecbdc152bde7224430169ab68b18e1028f9e0ae
Dac099 <davidcejazapata@gmail.com>
2021-07-19 21:32:33 -0500

    insertion/selection sort

--- algorithms/problem3.js
+++ algorithms/problem3.js
@@ -4,30 +4,25 @@
  */
 let l1 = [1,2,4];
 let l2 = [1,3,4];
+
 var mergeTwoLists = function(l1, l2) {
+    let controlLength = l1.length;
     for(let i = 0; i < l2.length; i++){
-        l1.push(l2[i]);
+        l1[controlLength + i] = l2[i];
     }
 
     //Ordenamiento por insercion [1,1,2,3,4,4]
-    //recorrer el arreglo
-        //El primer elemento se toma como el incio del arreglo ordenado
-        //Se pasa al siguiente elemento y lo comparamos con el arreglo anterior
-            //Si es menor 
-                //lo guardamos en la variable key
-                //el elemento con el que se comparo lo copiamos en la pocision del elemento que estamos examinando
-                //a la variable kay la asinamos en la posicion de el elmento con el que lo comparamos
-    let sorted = [];
-    for(let i = 0; i < l1.length; i++){
-        let key = l1[i + 1]; 
-        let posicion = indice;
-        for(let j = i; j >= 0; j--){
-            if(key < l1[j]){
-                l1[i + 1] = l1[j];
-            }
+    for(let i =  1; i < l1.length; i++){
+        let position = i;
+        let value = l1[i];
+        while(position > 0 && value < l1[position - 1]){
+            l1[position] = l1[position - 1];
+            position--;
         }
-    }   
+        l1[position] = value;
+    }
 
+    return l1;
 };
 
 console.log(mergeTwoLists(l1,l2));
